958e7e83bad0c5fc95ac0ccf6337477512082355
hugh <hhoshaughnes@wisc.edu>
2024-06-09 23:11:46 -0700

    add readme

---
+++ README.md
@@ -0,0 +1,2 @@
+<h1> SUPER COOL EPIC YAYAYAYAYA</h1>
+<p>drew eats poop</p>
---
+++ TrieNode.java
@@ -0,0 +1,5 @@
+public class TrieNode {
+    private HashMap<Character, TrieNode> children;
+    private String content;
+    private boolean isWord;
+}
